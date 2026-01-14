40b9154d2a187b77d8a5867bf632f64c71145b5f
ViniciusPaulo <viniciuspaulo0703@gmail.com>
2026-01-14 15:49:32 -0300

    feat: estiliza badges de tecnologias e adiciona validações no JS

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# meu-portifolio
+Meu Site de Apresentação/Portifólio
+
+Descrição: Esse projeto é apenas um site de apresentação para recrutadores e também para colocar em prática programar sites reais que serão usados.
+
+Tecnologias: HTML5, CSS3 (Flexbox/Grid), JavaScript (DOM).
+
+Como rodar: "Basta abrir o arquivo index.html no navegador".
--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
-// 1. Selecionamos o fomulário no HTML usando o ID que você criou.
+// Selecionamos o fomulário no HTML usando o ID que você criou.
 const form = document.getElementById("contact-form");
 
-// 2. Ouvimos o evento de "submit" (quando o usuário clica no botão).
+// Ouvimos o evento de "submit" (quando o usuário clica no botão).
 form.addEventListener("submit", function (event) {
   // 3.Impedimos o navegador de recarregar a página.
   event.preventDefault();
 
-  // 4. Pegamos os valores dos campos.
-  const nome = document.getElementById("nome").value;
+  // Pegamos os valores dos campos.
+  const nome = document.getElementById("nome").value.trim(); // .trim() remove espaços vazios
   const email = document.getElementById("email").value;
 
-  // 5.Exibimos um feedback para o usuário.
+    if(nome.length < 3) {
+        alert("Por favor, digite um nome válido (mínimo 3 caracteres).")
+        return; //Interrompe a execução aqui
+    }
+
+  // Exibimos um feedback para o usuário.
   alert(
     `Olá ${nome}, sua mensagem foi enviada com sucesso para o e-mail: ${email}`
   );

155d1bc21dad4af83cfdd8415d98fd2a6da79dc5
ViniciusPaulo <viniciuspaulo0703@gmail.com>
2026-01-14 17:27:25 -0300

    style: adiciona foto de perfil com layout responsivo e favicon

--- js/main.js
+++ js/main.js
@@ -10,10 +10,10 @@ form.addEventListener("submit", function (event) {
   const nome = document.getElementById("nome").value.trim(); // .trim() remove espaços vazios
   const email = document.getElementById("email").value;
 
-    if(nome.length < 3) {
-        alert("Por favor, digite um nome válido (mínimo 3 caracteres).")
-        return; //Interrompe a execução aqui
-    }
+  if (nome.length < 3) {
+    alert("Por favor, digite um nome válido (mínimo 3 caracteres).");
+    return; //Interrompe a execução aqui
+  }
 
   // Exibimos um feedback para o usuário.
   alert(
@@ -27,33 +27,33 @@ form.addEventListener("submit", function (event) {
 lucide.createIcons();
 
 const meusProjetos = [
-    {
-        titulo: "Portifólio Pessaol",
-        descricao: "Site moderno para apresentação de projetos.",
-        techs: ["HTML", "CSS", "JS"],
-        link: "#"
-    },
-    {
-        titulo: "Calculadora de Gorjetas",
-        descricao: "Aplicação simples para praticar lógica JS.",
-        techs: ["JS", "CSS"],
-        link: "#"
-    }
+  {
+    titulo: "Portifólio Pessaol",
+    descricao: "Site moderno para apresentação de projetos.",
+    techs: ["HTML", "CSS", "JS"],
+    link: "#",
+  },
+  {
+    titulo: "Calculadora de Gorjetas",
+    descricao: "Aplicação simples para praticar lógica JS.",
+    techs: ["JS", "CSS"],
+    link: "#",
+  },
 ];
 
-const grid = document.querySelector('.projects-grid');
+const grid = document.querySelector(".projects-grid");
 
 // Função para criar os cards automaticamente
-meusProjetos.forEach(projetos => {
-    const card = `
+meusProjetos.forEach((projetos) => {
+  const card = `
         <article class="project-card">
             <h3>${projeto.titulo}</h3>
             <p>${projeto.descricao}</p>
             <div class="techs">
-                ${projeto.techs.map(t => `<span>${t}</span>`).join('')}
+                ${projeto.techs.map((t) => `<span>${t}</span>`).join("")}
             </div>
             <a href="${projeto.link}">Ver Repositório</a>
         </article>
      `;
-     grid.innerHTML += card;
+  grid.innerHTML += card;
 });
